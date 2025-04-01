976490a6c868ab155d81b91f3a2aa6254200bbe1
Reinaldo_Cordeiro80 <reinaldofc80@gmail.com>
2025-04-01 11:49:06 -0300

    Criarção do Front da tela publicação

--- SIPROWEBPUBLICACAO/Controllers/PublicacaoController.cs
+++ SIPROWEBPUBLICACAO/Controllers/PublicacaoController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SIPROSHARED.Filtro;
 
 namespace SIPROWEBPUBLICACAO.Controllers
 {
+    [AutorizacaoTokenAttribute("ADM", "JULGAMENTO_1")]
     public class PublicacaoController : Controller
     {
         public IActionResult Publicacao()
--- SIPROWEBPUBLICACAO/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SIPROWEBPUBLICACAO.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
