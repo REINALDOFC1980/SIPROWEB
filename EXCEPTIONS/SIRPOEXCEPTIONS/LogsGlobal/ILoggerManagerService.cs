a313a9fbf09137eb06dc41804e5e0d3d9b5387f0
Reinaldo_Cordeiro80 <reinaldofc80@gmail.com>
2025-05-23 09:40:22 -0300

    LogGlobal

---
+++ EXCEPTIONS/SIRPOEXCEPTIONS/LogsGlobal/ILoggerManagerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIRPOEXCEPTIONS.Log
+{
+    public interface ILoggerManagerService
+    {
+        void LogInfo(string message, params object[]  args);  // 🔥 Sobrecarga
+        void LogWarn(string message);
+        void LogDebug(string message);
+        void LogError(string message);
+    }
+}
+
---
+++ EXCEPTIONS/SIRPOEXCEPTIONS/LogsGlobal/LoggerManagerService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIRPOEXCEPTIONS.Log
+{
+    public class LoggerManagerService : ILoggerManagerService
+    {
+        private readonly ILogger<LoggerManagerService> _logger;
+
+        public LoggerManagerService(ILogger<LoggerManagerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogDebug(string message)
+        {
+            _logger.LogDebug(message);
+        }
+
+
+        public void LogError(string message)
+        {
+            _logger.LogError(message);
+        }
+
+
+        public void LogInfo(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+
+        public void LogWarn(string message)
+        {
+            _logger.LogWarning(message);
+        }
+
+    }
+}
